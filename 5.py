ac7a0d3f1a7b04b2b834cea0cbe203bd28443e08
Anton Kan <freelead@gmail.com>
2025-12-22 23:03:43 -0500

    Replaced dict with numpy array

--- 5.py
+++ 5.py
@@ -1,21 +1,21 @@
-class Solution(object):
-    checked = {}
+import numpy as np
 
+class Solution(object):
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        pal = {}
         n = len(s)
+        pal = np.zeros((n, n))
         max_i = 0
         max_j = 0
         max_diff = 0
         for i in range(0, n):
-            pal[(i, i)] = True
+            pal[i, i] = True
         for i in range(0, n-1):
-            pal[(i, i+1)] = s[i] == s[i+1]
-            if pal[(i, i+1)]:
+            pal[i, i+1] = s[i] == s[i+1]
+            if pal[i, i+1]:
                 max_i = i
                 max_j = i+1
                 max_diff = 1
@@ -23,16 +23,15 @@ class Solution(object):
             for i in range(0, n-diff):
                 j = i+diff
                 if (s[i] == s[j]) and (pal[(i+1, j-1)]):
-                    pal[(i, j)] = True
+                    pal[i, j] = True
                     if (diff > max_diff):
                         max_i = i
                         max_j = j
                         max_diff = diff
                 else:
-                    pal[(i, j)] = False
+                    pal[i, j] = False
         return s[max_i:max_j+1]
 
-    
 print(Solution().longestPalindrome("babad"))
 print(Solution().longestPalindrome("cbbd"))
 print(Solution().longestPalindrome("cbb"))
